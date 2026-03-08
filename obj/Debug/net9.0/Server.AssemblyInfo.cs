82228a859aca1976cc73fc7f89900c28d5a3aedf
wije51 <wijethibanda@gmail.com>
2026-03-08 21:22:26 +0530

    Add Kali Linux to AI & Dev Tools

--- obj/Debug/net9.0/Server.AssemblyInfo.cs
+++ obj/Debug/net9.0/Server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a12ab31ee7c9333a7b1da6ad2a776ac64a14e80")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
